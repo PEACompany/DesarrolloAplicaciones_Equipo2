a7c2d4861fab32831937b2c3bd673e153e82adc9
Francisco Rodriguez <42101163+FranciscoRdz2001@users.noreply.github.com>
2023-03-07 18:09:50 -0800

    Practica 2 Moviles

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+
+
+export default class Header extends Component{
+    render(){
+        return <div style={{backgroundColor: "grey", clear: 'both' }}>
+            <h4 style={{color: 'white'}} >Texto 1</h4>
+        </div>
+    }
+}
