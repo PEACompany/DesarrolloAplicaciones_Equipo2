9b74fdbde165c06d5be71db650b8e323250b6e9e
CATAAA233 <carlosdanielcatalan1o@gmail.com>
2023-03-11 14:18:30 -0800

    adding image

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,11 +1,12 @@
 import { Component } from "react";
-
-
+import feliz from './../../assets/img/feliz.png'
 
 export default class Header extends Component {
     render() {
         return <div style={{ display: "flex", justifyContent: "center", alignItems:"center", height: "10rem"}}>
-            <div className="Image"></div>
+            <div className="Image">
+                <img style={{width:'5rem', height: '5rem'}} src={feliz} alt="" />
+            </div>
         </div>
     }
 }
